5a119d63b902e8d9a7fd050a55a87cb3b86b5dd7
NARUTOisthebest12 <stavya.adlakha@gmail.com>
2025-11-12 07:01:59 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import turtle
-turtle.Screen().bgcolor("Orange")
-sc = turtle.Screen()
-sc.setup(400,300)
-turtle.title("welcome to turtle window")
+num = 17
+print("Table of 17")
+for i in range(1,111) :
+    mul = num*i
+    print("17 x %d = %d" % (i,mul))
